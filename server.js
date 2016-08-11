1e9d36e74d0431973299e5c2e0a7d203aad98c70
jdavis218 <jennadavis92@gmail.com>
2016-08-11 16:14:33 -0700

    add gitignore

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("views", path.resolve(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+var db;
+MongoClient.connect('mongodb://jennafavorites:Rileylove98!@ds021333.mlab.com:21333/favorite-places', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, function() {
+    console.log('listening on 3000');
+  });
+
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + './client/index.html')
+});
+
+app.get('/favorites', function(req, res) {
+  db.collection('favorites').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    res.render('index.ejs', {favorites: result});
+  });
+});
+
+app.get('/stylefav', function(req, res) {
+  res.sendFile('/Users/jenna/Desktop/codesmith/ExploreYourCity/views/stylefav.css');
+});
+
+app.post('/favorites', function(req, res) {
+  var placeName = req.body.place;
+  console.log(req.body);
+  console.log(placeName);
+  db.collection('favorites').save(req.body, (err, results) => {
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/favorites');
+  });
+});
